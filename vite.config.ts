b727502f9d99cd93f69475baf23d8574ada863ae
Kalakota Venkat Ramana <venkatramana.kalakota@gmail.com>
2025-10-26 14:16:57 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // --- ADD THIS LINE ---
+  base: '/capstone1/', 
+  // ---------------------
+})
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
